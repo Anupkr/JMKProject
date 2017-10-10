621e0231b339af8d20c3e4ce9fb13afb649ce0a3
anup <anup@anup_pc>
2017-10-10 22:53:42 +0530

    Add Container Service

---
+++ JMKProject/src/main/java/com/jmk/service/ContainerService.java
@@ -0,0 +1,23 @@
+package com.jmk.service;
+
+import com.jmk.beans.Container;
+import java.util.List;
+
+/**
+ *
+ * @author anup
+ */
+public interface ContainerService
+{
+
+    /**
+     * 
+     * @param container
+     * @return
+     */
+    public String createContainer(Container container);
+    /**
+     * @return 
+     */
+    public List<Container> getAllContainer();
+}
